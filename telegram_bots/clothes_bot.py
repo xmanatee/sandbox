27448a5430b10311be5af8433e7d6ac7d496593e
xmanatee <xmanatee.one@gmail.com>
2017-12-18 03:37:36 +0300

    qfix

--- telegram_bots/clothes_bot.py
+++ telegram_bots/clothes_bot.py
@@ -32,7 +32,6 @@ un_to_chat_id = {}
 chat_id_to_photo_ids_dir = "./data/photos"
 chat_id_to_photo_ids = defaultdict(set)
 
-
 def init():
     global admin_uns, un_to_chat_id, chat_id_to_photo_ids
 
@@ -91,6 +90,12 @@ def update_photos(chat_id, photos, reset=False):
         json.dump(list(chat_id_to_photo_ids[chat_id]), f)
 
 
+def start(bot, update):
+    update.message.reply_text("Hi! I'm Clothes_Bot!\n"
+                              "Send photos and i'll keep them in draft.\n"
+                              "Type /review and i'll send the draft for review\n"
+                              "Type /reset and i'll reset the draft\n")
+
 def authorize(bot, update):
     un = update.message.from_user.username
     from_chat_id = update.message.chat.id
@@ -140,9 +145,10 @@ def reset(bot, update):
     if not chat_id_to_photo_ids[from_chat_id]:
         update.message.reply_text("nothing to reset")
                                   # reply_markup=ReplyKeyboardRemove())
-    else:
-        update.message.reply_text("photo buffer reset")
-                                  # reply_markup=ReplyKeyboardRemove())
+        return True
+
+    update.message.reply_text("photo buffer reset")
+                              # reply_markup=ReplyKeyboardRemove())
 
     update_photos(from_chat_id, [], reset=True)
 
@@ -155,16 +161,17 @@ def review(bot, update):
 
     if not chat_id_to_photo_ids[from_chat_id]:
         update.message.reply_text("nothing to review")
-    else:
-        media_group = [InputMediaPhoto(id) for id in chat_id_to_photo_ids[from_chat_id]]
+        return True
 
-        logging.info("sending for review to {}".format(admin_uns))
+    media_group = [InputMediaPhoto(id) for id in chat_id_to_photo_ids[from_chat_id]]
 
-        for admin_un in admin_uns:
-            admin_chat_id = un_to_chat_id[admin_un]
-            bot.sendMediaGroup(admin_chat_id, media_group)
-            bot.sendMessage(admin_chat_id, "from @{}".format(un),
-                            reply_markup=admin_keyboard)
+    logging.info("sending for review to {}".format(admin_uns))
+
+    for admin_un in admin_uns:
+        admin_chat_id = un_to_chat_id[admin_un]
+        bot.sendMediaGroup(admin_chat_id, media_group)
+        bot.sendMessage(admin_chat_id, "from @{}".format(un),
+                        reply_markup=admin_keyboard)
 
     photos_num = len(chat_id_to_photo_ids[from_chat_id])
     bot.sendMessage(from_chat_id, "{} photos sent for review".format(photos_num))
@@ -189,12 +196,13 @@ def main():
 
     dp = updater.dispatcher
 
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('review', review))
+    dp.add_handler(CommandHandler('reset', reset))
+
     dp.add_handler(MessageHandler(Filters.text, authorize))
 
     dp.add_handler(MessageHandler(Filters.photo, photo))
-
-    dp.add_handler(CommandHandler('review', review))
-    dp.add_handler(CommandHandler('reset', reset))
 
     # dp.add_handler(MessageHandler(Filters.all, log))
 
